a922bbb240630310ce09933983fa1bdac2ab720b
BroFro69 <48029062+BroFro69@users.noreply.github.com>
2021-01-27 13:42:13 -0700

    Add files via upload

---
+++ outLab1/src/outLab1/Test.java
@@ -0,0 +1,12 @@
+package outLab1;
+
+public class Test {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("Hello, World!");
+		System.out.println("\nThis is a test of the repo.");
+		
+	}
+	
+}
